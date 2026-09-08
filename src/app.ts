579f7f3516fb5a1b98f257121441486a66a6f8d6
Anton Bondar <ilogoua@gmail.com>
2026-09-08 19:00:18 +0200

    Set up Express and MongoDB connection

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-console.log('Basic Node + TS scaffolding');
+import { connectToDatabase } from './db/index.ts';
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT ?? 3000;
+
+app.get('/', (_req, res) => {
+  res.json({ message: 'eCommerce API is running' });
+});
+
+await connectToDatabase();
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
